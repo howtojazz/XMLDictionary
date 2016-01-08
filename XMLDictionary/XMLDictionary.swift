634a12d1e8400070220d2bc2015f554b6fa3229e
howtojazz <swchowtojazz@gmail.com>
2016-01-08 23:53:00 +0900

    added license description

--- XMLDictionary/XMLDictionary.swift
+++ XMLDictionary/XMLDictionary.swift
@@ -1,12 +1,34 @@
 //
 //  XMLDictionary.swift
-//  XMLTest
 //
-//  Created by SeokWon Cheul on 2016. 1. 7..
+//  Version 1.4
 //
+//  Created by Nick Lockwood on 15/11/2010.
+//  Copyright 2010 Charcoal Design. All rights reserved.
 //
+//  Get the latest version of XMLDictionary from here:
+//
+//  https://github.com/nicklockwood/XMLDictionary
+//
+//  This software is provided 'as-is', without any express or implied
+//  warranty.  In no event will the authors be held liable for any damages
+//  arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//  claim that you wrote the original software. If you use this software
+//  in a product, an acknowledgment in the product documentation would be
+//  appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be
+//  misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
 
-import UIKit
+import Foundation
 
 enum XMLDictionaryAttributesMode {
     case XMLDictionaryAttributesModePrefixed //default
@@ -14,18 +36,18 @@ enum XMLDictionaryAttributesMode {
     case XMLDictionaryAttributesModeUnprefixed
     case XMLDictionaryAttributesModeDiscard
 }
+
 enum XMLDictionaryNodeNameMode {
     case XMLDictionaryNodeNameModeRootOnly //default
     case XMLDictionaryNodeNameModeAlways
     case XMLDictionaryNodeNameModeNever
-    
 }
 
-let XMLDictionaryAttributesKey : NSString   = "__attributes"
-let XMLDictionaryCommentsKey : NSString     = "__comments"
-let XMLDictionaryTextKey : NSString         = "__text"
-let XMLDictionaryNodeNameKey : NSString     = "__name"
-let XMLDictionaryAttributePrefix : NSString = "_"
+let XMLDictionaryAttributesKey  : NSString  = "__attributes"
+let XMLDictionaryCommentsKey    : NSString  = "__comments"
+let XMLDictionaryTextKey        : NSString  = "__text"
+let XMLDictionaryNodeNameKey    : NSString  = "__name"
+let XMLDictionaryAttributePrefix: NSString  = "_"
 
 class XMLDictionaryParser : NSObject, NSCopying, NSXMLParserDelegate {
     
